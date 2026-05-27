54932e4a990109d9f720c2614a63219056142c2b
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-05-27 13:31:04 +0200

    implement POST structure

---
+++ WebApplication1/WebApplication1/DTOs/BedAssignmentPostDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.DTOs;
+public class BedAssignmentPostDto
+{
+    [Required]
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string BedType { get; set; } = null!;
+    [Required]
+    [StringLength(300)]
+    public string Ward { get; set; } = null!;
+}
