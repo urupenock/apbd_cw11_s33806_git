180b67b1fa1f8686ad2eaae3951363963273cf7f
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-05-27 13:29:00 +0200

    implement GET structure

---
+++ WebApplication1/WebApplication1/DTOs/PatientGetDto.cs
@@ -0,0 +1,49 @@
+﻿namespace WebApplication1.DTOs;
+public class PatientGetDto
+{
+    public string Pesel { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public int Age { get; set; }
+    public string Sex { get; set; } = null!; 
+    public List<AdmissionDto> Admissions { get; set; } = new();
+    public List<BedAssignmentDto> BedAssignments { get; set; } = new();
+}
+public class AdmissionDto
+{
+    public int Id { get; set; }
+    public DateTime AdmissionDate { get; set; }
+    public DateTime? DischargeDate { get; set; }
+    public WardDto Ward { get; set; } = null!;
+}
+public class WardDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+}
+public class BedAssignmentDto
+{
+    public int Id { get; set; }
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    public BedDto Bed { get; set; } = null!;
+}
+public class BedDto
+{
+    public int Id { get; set; }
+    public BedTypeDto BedType { get; set; } = null!;
+    public RoomDto Room { get; set; } = null!;
+}
+public class BedTypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+}
+public class RoomDto
+{
+    public string Id { get; set; } = null!;
+    public bool HasTv { get; set; }
+    public WardDto Ward { get; set; } = null!;
+}
